36ac666297866303514b0e111e8caf8f05b28b57
Matthieu Coudron <mcoudron@hotmail.com>
2020-12-15 09:26:35 +0100

    pinned nixpkgs

--- pinned_nixpkgs.nix
+++ pinned_nixpkgs.nix
@@ -6,20 +6,17 @@ let
           ip = dontCheck hold.ip;
           bytebuild = dontCheck hold.bytebuild;
 
-          # can be released on more recent nixplks
-          # wide-word = doJailbreak (hold.wide-word);
-
           # for newer nixpkgs (March 2020)
           # base-compat = doJailbreak (hold.base-compat);
           # time-compat = doJailbreak (hold.time-compat);
-          mptcp-pm = (overrideSrc hold.mptcp-pm {
-            src = prev.fetchFromGitHub {
-              owner = "teto";
-              repo = "mptcp-pm";
-              rev = "4087bd580dcb08919e8e3bc78ec3b25d42ee020d";
-              sha256 = "sha256-MiXbj2G7XSRCcM0rnLrbO9L5ZFyh6Z3sPtnH+ddInI8=";
-            };
-          });
+          # mptcp-pm = (overrideSrc hold.mptcp-pm {
+          #   src = prev.fetchFromGitHub {
+          #     owner = "teto";
+          #     repo = "mptcp-pm";
+          #     rev = "4087bd580dcb08919e8e3bc78ec3b25d42ee020d";
+          #     sha256 = "sha256-MiXbj2G7XSRCcM0rnLrbO9L5ZFyh6Z3sPtnH+ddInI8=";
+          #   };
+          # });
           netlink = (overrideSrc hold.netlink {
             # src = builtins.fetchGit {
             #   # url = https://github.com/ongy/netlink-hs;
@@ -32,27 +29,18 @@ let
               sha256 = "sha256-qopa1ED4Bqk185b1AXZ32BG2s80SHDSkCODyoZfnft0=";
             };
           });
-
-          # TODO change source
-          # bitset = pkgs.haskell.lib.overrideSrc hold.bitset { src = pkgs.fetchFromGithub {
-          #     owner = "teto";
-          #     repository = "bitset";
-          #     rev = "upgrade";
-          #     sha256 = "0j0hrzr9b57ifwfhggpzm43zcf6wcsj8ffxv6rz7ni7ar1x96x2c";
-          #   };
-          # };
-
         };
       };
   };
 
+  nixpkgsRev = "e9158eca70ae59e73fae23be5d13d3fa0cfc78b4";
   # pinned nixpkgs before cabal 3 becomes the default else hie fails
-  nixpkgs = import <nixpkgs>
-  # nixpkgs = import (builtins.fetchTarball {
-  #     name = "before-libc-update";
-  #     url = "https://github.com/nixos/nixpkgs/archive/fa7445532900f2555435076c1e7dce0684daa01a.tar.gz";
-  #     sha256 = "1hbf7kmbxmd19hj3kz9lglnyi4g20jjychmlhcz4bx1limfv3c3r";
-  # })
+  # nixpkgs = import <nixpkgs>
+  nixpkgs = import (builtins.fetchTarball {
+      name = "nixos-unstable";
+      url = "https://github.com/nixos/nixpkgs/archive/${nixpkgsRev}.tar.gz";
+      sha256 = "0cnmvnvin9ixzl98fmlm3g17l6w95gifqfb3rfxs55c0wj2ddy53";
+  })
   {
     overlays = [ overlay]; config = {allowBroken = true;};
   };
