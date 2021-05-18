a647a6177ce335b8a5b659bb8702f896a0144756
Matthieu Coudron <mcoudron@hotmail.com>
2021-05-18 20:40:55 +0200

    WIP

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    replica.url = "github:teto/REPLica/nix";
+    replica.url = "github:berewt/REPLica";
 
     # temporary until this gets fixed upstream
     # poetry.url = "github:teto/poetry2nix/fix_tag";
@@ -15,6 +15,27 @@
 
   outputs = { self, nixpkgs, flake-utils, poetry, replica, ... }@inputs:
     flake-utils.lib.eachSystem ["x86_64-linux"] (system: let
+
+      haskellOverlay = hnew: hold: with pkgs.haskell.lib; {
+
+        # TODO override Frames
+        ip = unmarkBroken (dontCheck hold.ip);
+        bytebuild = unmarkBroken (dontCheck hold.bytebuild);
+        wide-word = unmarkBroken (dontCheck hold.wide-word);
+
+        netlink = (overrideSrc hold.netlink {
+          # src = builtins.fetchGit {
+          #   # url = https://github.com/ongy/netlink-hs;
+          #   url = https://github.com/teto/netlink-hs;
+          # };
+          src = pkgs.fetchFromGitHub {
+            owner = "teto";
+            repo = "netlink-hs";
+            rev = "090a48ebdbc35171529c7db1bd420d227c19b76d";
+            sha256 = "sha256-qopa1ED4Bqk185b1AXZ32BG2s80SHDSkCODyoZfnft0=";
+          };
+        });
+      };
 
       compilerVersion = "8104";
       pkgs = nixpkgs.legacyPackages."${system}";
@@ -38,37 +59,47 @@
     in rec {
 
       # packages.mptcpanalyzer = nixpkgs.legacyPackages.x86_64-linux.hello;
-      packages.mptcpanalyzer = pkgs.stdenv.mkDerivation rec {
-        name = "mptcpanalyzer";
-        version = "0.1";
-        src = ./.;
+      # callCabal2nixWithOptions
+      packages.mptcpanalyzer = myHaskellPackages.callCabal2nix "mptcpanalyzer" ./. {};
 
-      buildInputs = with pkgs; [
-        cairo # for chart-cairo
-        dhall-json  # for dhall-to-json
-        glib
-        hsEnv
-        pkg-config
-        zlib
-        replica.packages."${system}".build
-      ];
+      # packages.mptcpanalyzer = pkgs.stdenv.mkDerivation rec {
+      #   name = "mptcpanalyzer";
+      #   version = "0.1";
+      #   src = ./.;
+      #   buildInputs = with pkgs; [
+      #     cairo # for chart-cairo
+      #     dhall-json  # for dhall-to-json
+      #     glib
+      #     hsEnv
+      #     pkg-config
+      #     zlib
+      #   ];
 
-      # see https://discourse.nixos.org/t/shared-libraries-error-with-cabal-repl-in-nix-shell/8921/9
-      LD_LIBRARY_PATH = nixpkgs.lib.makeLibraryPath buildInputs;
+      #   # see https://discourse.nixos.org/t/shared-libraries-error-with-cabal-repl-in-nix-shell/8921/9
+      #   LD_LIBRARY_PATH = nixpkgs.lib.makeLibraryPath buildInputs;
 
-    # # export HIE_HOOGLE_DATABASE=$NIX_GHC_DOCDIR as DOCDIR doesn't exist it won't work
-      # export ASAN_OPTIONS="log_path=./test.log:abort_on_error=1"
-      # export UBSAN_OPTIONS=print_stacktrace=1
-    shellHook = ''
-      # check if it's still needed ?
-      export NVIM_LOG_FILE=/tmp/log
+      # # # export HIE_HOOGLE_DATABASE=$NIX_GHC_DOCDIR as DOCDIR doesn't exist it won't work
+      #   # export ASAN_OPTIONS="log_path=./test.log:abort_on_error=1"
+      #   # export UBSAN_OPTIONS=print_stacktrace=1
+      #   shellHook = ''
+      #     # check if it's still needed ?
+      #     export NVIM_LOG_FILE=/tmp/log
 
-      ulimit -c unlimited
-    '';
-
-      };
+      #     ulimit -c unlimited
+      #   '';
+      # };
 
       defaultPackage = packages.mptcpanalyzer;
+
+      devShell = pkgs.mkShell {
+        name = "dev-shell";
+        buildInputs = [
+          # poetry.packages."${system}".poetry
+          defaultPackage.inputDerivation
+          replica.packages."${system}".build
+          inputs.hls.packages."${system}"."haskell-language-server-${compilerVersion}"
+        ];
+      };
 
     });
 }
